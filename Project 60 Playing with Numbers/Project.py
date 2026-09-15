6b3c0d10c79f9feac3721095ddf4463a2ee98da7
Amitt <neeta79.ns@gmail.com>
2026-09-15 19:26:11 +0530

    Project60

---
+++ Project 60 Playing with Numbers/Project.py
@@ -0,0 +1,8 @@
+import math
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+lcm = math.lcm(num1, num2)
+
+print(f"The LCM of {num1} and {num2} is: {lcm}")
